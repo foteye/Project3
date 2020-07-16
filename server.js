4940d67d72046e82936e5863e1b1697ef9d84f3a
Foti.Mougos <foti.mougos@gmail.com>
2020-07-16 19:45:43 +1000

    fixing deployment

--- server.js
+++ server.js
@@ -9,16 +9,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("client/public"));
-app.use(
-  session({
-    secret: "thisisaverysecretsecret",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+
+// app.use(
+//   session({
+//     secret: "thisisaverysecretsecret",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
 if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
   app.use(express.static("client/public"));
 }
 
